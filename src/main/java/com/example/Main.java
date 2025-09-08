3a884f4e5c97921bd5d5a7a5efb9306d6fca3dea
Mats Rönnqvist <mats.f.ronnqvist@gmail.com>
2025-09-08 20:07:44 +0200

    Test

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -2,8 +2,14 @@ package com.example;
 
 import com.example.api.ElpriserAPI;
 
+import java.time.LocalDate;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         ElpriserAPI elpriserAPI = new ElpriserAPI();
+        LocalDate idag = LocalDate.now();
+        List<ElpriserAPI.Elpris> dagensPriser = elpriserAPI.getPriser(idag, ElpriserAPI.Prisklass.SE3);
+        dagensPriser.stream().limit(3).forEach(pris -> System.out.printf("Tid: %s, Pris: %.4f SEK/kWh\n", pris.timeStart().toLocalTime(), pris.sekPerKWh()));
     }
 }
